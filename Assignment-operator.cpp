b3483054ac9c44f871b733a1ff7fff0c74780f00
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-11-09 21:20:03 +0530

    Add files via upload

---
+++ Assignment-operator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //{ =  /=  -=  += %=}
+
+    int a = 10;
+    int b = 10;
+    int c = 10;
+    int d = 10;
+
+    a += 10;
+    cout << a << endl;
+
+    b -= 10;
+    cout << b << endl;
+
+    c /= 10;
+    cout << c << endl;
+
+    d %= 10;
+    cout << d << endl;
+}
