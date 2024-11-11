d5cf86c21589010785ed78a505ebbdca10292935
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-11-11 22:01:47 +0530

    Add files via upload

---
+++ comparision.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+//{==,>,<,>=,<=,!=}
+
+int main()
+{
+    //comparision operator
+    // 0 or 1
+
+   int a, b;
+   cout<<"Enter a : ";
+   cin>>a;
+
+   cout<<"Enter b : ";
+   cin>>b;
+
+   // ==
+   if(a==b)
+   cout<<"Yes";
+   else
+   cout<<"No";
+   cout<<endl;
+
+    // >
+   if(a>b)
+   cout<<"Yes";
+   else
+   cout<<"No";
+   cout<<endl;
+
+    // <
+   if(a<b)
+   cout<<"Yes";
+   else
+   cout<<"No";
+   cout<<endl;
+
+    // >=
+   if(a>=b)
+   cout<<"Yes";
+   else
+   cout<<"No";
+   cout<<endl;
+
+    // <=
+   if(a <=b)
+   cout<<"Yes";
+   else
+   cout<<"No";
+   cout<<endl;
+
+    // !=
+   if(a!=b)
+   cout<<"Yes";
+   else
+   cout<<"No";
+   cout<<endl;
+
+
+}
