aa3cf95aa51dd1455342341306cfc0eda8e61a99
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-11-12 22:52:03 +0530

    Add files via upload

---
+++ And-oprator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter a : ";
+    cin>>a;
+
+     cout<<"Enter b : ";
+    cin>>b;
+
+     cout<<"Enter c : ";
+    cin>>c;
+
+    // && oprator
+    if(a>b && a>c)
+    cout<<"yes";
+    else
+    cout<<"No";
+}
