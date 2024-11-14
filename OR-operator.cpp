726a7868b54af255cc350c7aca9c1c8654b9fb99
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-11-14 21:40:27 +0530

    Add files via upload

---
+++ OR-operator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char name ='a';
+
+    if(name=='a' || name=='e' || name=='i' ||name=='o'||name=='u')
+    cout<<"Vovel";
+    else
+    cout<<"Consonant";
+}
